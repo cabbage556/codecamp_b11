42f4837c66b10bb349512a5f9aab0f961521320a
HUMBLE25 <wlsrua1007@gmail.com>
2023-01-11 13:47:08 +0900

    --

--- kimjingyeom/day02/template.js
+++ kimjingyeom/day02/template.js
@@ -8,7 +8,7 @@ import {
 function createUser({ email, resisterNumber, phoneNumber, loveSite }) {
   //   console.log(email);
   //1.이메일이 정상인지 확인(1-존재 여부,2-"@"포함여부)
-  const isValid = checkEmail({ email });
+  const isValid = checkEmail({ email: email });
   if (isValid === false) return;
   //2. 주민번호 블러처리
   const getNumber = makingBlur({ resisterNumber });
@@ -21,7 +21,7 @@ function createUser({ email, resisterNumber, phoneNumber, loveSite }) {
     loveSite,
   });
   //4.이메일에 가입환영 템플릿 전송하기
-  sendWelcomeTemplateToEmail(email, getTemplate); ///이부분복습
+  sendWelcomeTemplateToEmail({ email, getTemplate }); ///이부분복습
 }
 
 const email = "id@gmail";
