c41a2f6a18df05dccc8e539877326576319e252e
liberty556 <liberty556@gmail.com>
2023-01-12 20:23:59 +0900

    4일차 김태윤 제출

---
+++ kimtaeyun/day04/swagger/starbucks.swagger.js
@@ -0,0 +1,17 @@
+/**
+ * @swagger
+ * /starbucks:
+ *   get:
+ *     summary: 커피목록 가져오기
+ *     tags: [Coffee]
+ *     parameters:
+ *       - in: query
+ *         name:
+ *         type:
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ */
