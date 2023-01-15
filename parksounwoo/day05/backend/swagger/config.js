9865d5fb4ff6921529a3bcb63f3f3140f24925a0
sounwoo <mna800@naver.com>
2023-01-15 12:11:41 +0900

    5일차 박선우 제출

---
+++ parksounwoo/day05/backend/swagger/config.js
@@ -0,0 +1,10 @@
+export const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: '🐶 박선우 API 설명서',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./swagger/*.swagger.js'],
+};
