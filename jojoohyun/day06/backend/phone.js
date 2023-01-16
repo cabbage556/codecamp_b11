72f04e6da8b675937a8c526f3a26eec03cd5efbd
Jjoobob123 <273hur4747@gmail.com>
2023-01-16 21:30:15 +0900

    조주현 6일차 제출

---
+++ jojoohyun/day06/backend/phone.js
@@ -0,0 +1,43 @@
+import coolsms from "coolsms-node-sdk"
+const mysms = coolsms.default
+
+
+export function checkPhone(myphone){
+    if(myphone.length < 10 || myphone.length > 11){
+        console.log("에러 발생 !! 핸드폰 번호를 제대로 입력해주세요 !!!"); // early-exit 
+        return false
+    } else {
+        return true
+    }
+}
+
+export function getToken(){
+    const result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0")
+    console.log(result);
+    return result
+}
+
+export async function sendTokenToSMS(myphone, result){
+    const SMS_KEY = process.env.SMS_KEY;
+    const SMS_SECRET = process.env.SMS_SECRET;
+    const SMS_SENDER = process.env.SMS_SENDER;
+    
+    
+    const messageService = new mysms(SMS_KEY, SMS_SECRET)
+    const res = await messageService.sendOne({
+        to: myphone,
+        from: SMS_SENDER,
+        text:`[코드캠프] 안녕하세요?! 요청하신 인증번호는 ${result}입니다!`
+    })
+
+    // console.log(myphone + "번호로 인증번호 " + result + "를 전송합니다.");
+
+}
+// API를 실행했다.
+
+
+// createTokenOfPhone("01012345678") // 0101234567: 인자(argument)
+
+
+// export default function(){}
+// default 기본값으로 정해져 있다. import 뒤에 아무 함수나 적어도 기본으로 적용된다
---
+++ jojoohyun/day06/frontend/login/signup.js
@@ -0,0 +1,48 @@
+// 휴대폰 인증 토큰 전송하기
+const getValidationNumber = async () => {
+  document.querySelector('#ValidationInputWrapper').style.display = 'flex'
+  const phone1 = document.getElementById('PhoneNumber01').value;    
+  const phone2 = document.getElementById('PhoneNumber02').value;
+  const phone3 = document.getElementById('PhoneNumber03').value;
+
+  const myphone = phone1 + phone2 + phone3;
+
+  
+  axios.post('http://localhost:3000/tokens/phone', {
+      myphone,
+  }).then((res) => {
+      console.log(res.data);
+  });
+  console.log('인증 번호 전송')
+}
+
+// 회원 가입 API 요청
+const submitSignup = async () => {
+  const name = document.getElementById("SignupName").value
+  const personal = document.getElementById("SignupPersonal").value
+  const phone01 = document.getElementById("PhoneNumber01").value
+  const phone02 = document.getElementById("PhoneNumber02").value
+  const phone03 = document.getElementById("PhoneNumber03").value
+  const prefer = document.getElementById("SignupPrefer").value
+  const email = document.getElementById("SignupEmail").value
+  // const password = document.getElementById("SignupPwd").value
+  
+  const myphone = phone01 + phone02 + phone03;
+  console.log(myphone);
+  axios.post("http://localhost:3000/users", {
+    name,
+    personal,
+    prefer,
+    email,
+    myphone
+  }).then((res)=>{
+    console.log(res.data);
+  })
+
+
+  
+  
+  
+  console.log('회원 가입 이메일 전송')
+
+}
