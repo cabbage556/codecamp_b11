42bcfa20b39b8a2198f92e68496d129e07ee1aea
zynoobb <jw9491@gmail.com>
2023-01-19 19:36:40 +0900

    9일차 이진호 제출

---
+++ leejinho/day09/backend/email.js
@@ -0,0 +1,57 @@
+import 'dotenv/config'
+import { getToday } from "./utils.js"
+import nodemailer from 'nodemailer'
+
+export function checkEmail(email){
+    if(email === undefined || email.includes("@")=== false){
+        console.log("이메일 올바르지 않음")
+        return false
+    } else {
+        return true
+    }
+}
+
+// css 최신문법 사용하지 않기 // 각기 브라우저마다 적용하는 방식 다름
+export function makeTemplate(SignupName,SignupPhone,SignupPrefer) {
+    const myTemplate = `
+    <html>
+        <body>
+            <div>
+                <h1>${SignupName}님 가입을 환영합니다!!</h1>
+                <hr />
+                <div style="color:red;">이름 : ${SignupName}</div>
+                <div>전화번호 : ${SignupPhone}</div>
+                <div>좋아하는 사이트 : ${SignupPrefer}</div>
+                <div>가입일 : ${getToday()}</div>
+            </div>
+        </body>
+    </html>
+    `
+    return myTemplate
+}
+
+export async function sendTemplateToEmail(email, template){
+    const EMAIL_ID = process.env.EMAIL_ID
+    const EMAIL_PW = process.env.EMAIL_PW
+    
+    const transporter = nodemailer.createTransport({
+
+        service : "gmail",
+        auth : {
+            user : EMAIL_ID,
+            pass : EMAIL_PW
+        }
+    })
+
+    const res = await transporter.sendMail({
+        from: "jw9491@gmail.com",
+        to : email,
+        subject : "가입을 축하합니당",
+        html : template
+    })
+    console.log(res)
+
+
+
+    // console.log(`${email} 이메일로, 가입환영 탬플릿 ${template} 전송합니다.`)
+}
