dd2794c7f91eec810cb366935d8c4629f0459755
liberty556 <liberty556@gmail.com>
2023-01-26 19:50:22 +0900

    12일차 김태윤 제출

--- kimtaeyun/day12/backend/controllers/services/user.services.js
+++ kimtaeyun/day12/backend/controllers/services/user.services.js
@@ -3,54 +3,56 @@ import { formatPersonal } from "../../srcs/utils.js";
 import axios from "axios";
 import cheerio from "cheerio";
 
-export const createSaveUserDocument = async ({
-  name,
-  email,
-  personal,
-  prefer,
-  pwd,
-  phone,
-  og,
-}) => {
-  const user = await new User({
+export class UserService {
+  createSaveUserDocument = async ({
     name,
     email,
-    personal: formatPersonal({ personal }),
+    personal,
     prefer,
     pwd,
     phone,
     og,
-  }).save();
-  return user._id;
-};
+  }) => {
+    const user = await new User({
+      name,
+      email,
+      personal: formatPersonal({ personal }),
+      prefer,
+      pwd,
+      phone,
+      og,
+    }).save();
+    return user._id;
+  };
 
-export const getAllUsers = async () => {
-  // name, email, personal, prefer, phone, og만 골라서 가져오기
-  // _id는 제외하기
-  return await User.find().select([
-    "name",
-    "email",
-    "personal",
-    "prefer",
-    "phone",
-    "og",
-    "-_id",
-  ]);
-};
+  getAllUsers = async () => {
+    // name, email, personal, prefer, phone, og만 골라서 가져오기
+    // _id는 제외하기
+    return await User.find().select([
+      "name",
+      "email",
+      "personal",
+      "prefer",
+      "phone",
+      "og",
+      "-_id",
+    ]);
+  };
 
-export const getOgContent = async ({ prefer }) => {
-  const og = {};
-  const preferData = await axios.get(prefer);
-  const $ = cheerio.load(preferData.data);
-  $("meta").each((index, el) => {
-    if ($(el).attr("property") && $(el).attr("property").includes("og:")) {
-      const key = $(el).attr("property");
-      const value = $(el).attr("content");
+  getOgContent = async ({ prefer }) => {
+    const og = {};
+    const preferData = await axios.get(prefer);
+    const $ = cheerio.load(preferData.data);
+    $("meta").each((index, el) => {
+      if ($(el).attr("property") && $(el).attr("property").includes("og:")) {
+        const key = $(el).attr("property");
+        const value = $(el).attr("content");
 
-      if (key.includes("title")) og["title"] = value;
-      else if (key.includes("image")) og["image"] = value;
-      else if (key.includes("description")) og["description"] = value;
-    }
-  });
-  return og;
-};
+        if (key.includes("title")) og["title"] = value;
+        else if (key.includes("image")) og["image"] = value;
+        else if (key.includes("description")) og["description"] = value;
+      }
+    });
+    return og;
+  };
+}
--- kimtaeyun/day12/backend/controllers/user.controller.js
+++ kimtaeyun/day12/backend/controllers/user.controller.js
@@ -1,34 +1,30 @@
-import {
-  getAllUsers,
-  getOgContent,
-  createSaveUserDocument,
-} from "./services/user.services.js";
-import { checkTokenDocument } from "./services/token.services.js";
-import {
-  checkEmail,
-  createWelcomeTemplate,
-  sendWelcomeTemplateToEmail,
-} from "./services/email.services.js";
+import { UserService } from "./services/user.services.js";
+import { TokenService } from "./services/token.services.js";
+import { EmailService } from "./services/email.services.js";
 
 export class UserController {
   getUsers = async (_, res) => {
-    return res.status(200).send(await getAllUsers());
+    const userService = new UserService();
+    return res.status(200).send(await userService.getAllUsers());
   };
 
   signUpUser = async (req, res) => {
     const { name, email, personal, prefer, pwd, phone } = req.body;
+    const tokenService = new TokenService();
 
-    if (!(await checkTokenDocument({ phone }))) {
+    if (!(await tokenService.checkTokenDocument({ phone }))) {
       // 핸드폰 번호가 없거나, isAuth가 false라면 422 상태코드와 함께 에러 문구 반환
       return res.status(422).send("에러!! 핸드폰 번호가 인증되지 않았습니다.");
     }
 
-    if (!checkEmail({ email })) {
+    const emailService = new EmailService();
+    if (!emailService.checkEmail({ email })) {
       return res.status(422).send("에러!! 이메일 형식이 올바르지 않습니다.");
     }
 
-    const og = await getOgContent({ prefer });
-    const _id = await createSaveUserDocument({
+    const userService = new UserService();
+    const og = await userService.getOgContent({ prefer });
+    const _id = await userService.createSaveUserDocument({
       name,
       email,
       personal,
@@ -37,8 +33,12 @@ export class UserController {
       phone,
       og,
     });
-    const welcomeTemplate = createWelcomeTemplate({ name, phone, prefer });
-    sendWelcomeTemplateToEmail({ name, email, welcomeTemplate });
+    const welcomeTemplate = emailService.createWelcomeTemplate({
+      name,
+      phone,
+      prefer,
+    });
+    emailService.sendWelcomeTemplateToEmail({ name, email, welcomeTemplate });
 
     return res.status(200).send(_id);
   };
