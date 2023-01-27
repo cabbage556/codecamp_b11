fa36d9c2972ccadbd9e1f7192b5aca3b0e7befdc
liberty556 <liberty556@gmail.com>
2023-01-28 00:51:16 +0900

    13일차 수업: 의존성 주입, 제어 역전, 타입스크립트

---
+++ class/section07/07-03-typescript-private-readonly/03-protected.ts
@@ -0,0 +1,35 @@
+// // public, private, protected, readonly
+
+// class Monster2 {
+//   // power;                   => public, private, protected, readonly 중 하나라도 있으면 생략 가능
+
+//   // 생성자
+//   constructor(protected power) {
+//     // 클래스 내부에서 수정, 접근 가능 / 자식 클래스 내부에서 수정, 접근 가능
+//     // this.power = power;    => public, private, protected, readonly 중 하나라도 있으면 생략 가능
+//   }
+
+//   attack1 = () => {
+//     console.log("공격하자~");
+//     console.log("내 공격력은 " + this.power + "!!"); // 클래스 내부에서 접근 가능
+//     this.power = 30; // 클래스 내부에서 수정 가능
+//   };
+// }
+
+// class 공중몬스터2 extends Monster2 {
+//   attack2 = () => {
+//     console.log("공격하자~");
+//     console.log("내 공격력은 " + this.power + "!!"); // 자식 클래스 내부에서 접근 가능
+//     this.power = 30; // 자식 클래스 내부에서 수정 가능
+//   };
+// }
+
+// // 상속 => 몸통이 아예 다르다.
+// const myMonster22 = new 공중몬스터2(20);
+// myMonster22.attack1();
+// myMonster22.attack2();
+
+// // 클래스 밖에서 접근, 수정 불가능
+// // console.log(myMonster22.power);
+// // myMonster22.power = 10;
+// // console.log(myMonster22.power);
