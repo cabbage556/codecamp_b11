424e6fef7d1261bbe2ed2c85fe6add0d62c7830b
otter9459 <85683209+otter9459@users.noreply.github.com>
2023-02-10 09:51:19 +0900

    Initial commit

---
+++ src/stringPattern.js
@@ -0,0 +1,51 @@
+/*
+    문자열 패턴
+
+    문자열 pattern과 str이 주어집니다.
+    두 문자열이 입력된 패턴이 완전히 같은지 확인한 뒤,
+    같은 패턴일 경우 true를,
+    패턴이 다를 경우 false를 리턴해주세요.
+
+    - 문자열 pattern은 공백이 존재하지 않습니다.
+    - 문자열 str은 단어 사이에 공백이 존재합니다.
+    - pattern과 str 내 단어가 반복되는 횟수까지도 같아야 완전히 같은 패턴입니다.
+
+    입출력 예시
+    ------------------------------
+    input
+    ------------------------------
+
+    case1:
+      const pattern = 'abba'
+      const str = 'dog cat cat dog'
+      stringPattern(pattern, str)
+
+    case2:
+      const pattern = 'abcaba';
+      const str = 'cup ice coffee cup ice coffee';
+      stringPattern(pattern, str)
+
+    case3:
+      const pattern = 'abbab';
+      const str = 'a b b a b a b b a b';
+      stringPattern(pattern, str)
+
+    ------------------------------
+    output
+    ------------------------------
+
+    case1:
+      true
+
+    case2:
+      false
+      
+    case3:
+      false
+*/
+
+function stringPattern(pattern, str) {
+  // 여기에서 작업하세요.
+}
+
+module.exports = stringPattern;
